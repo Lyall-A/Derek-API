8d47eb1bdbd7fc817763fe7f139731753d47cf36
Lyall <me@lyall.lol>
2025-03-26 19:59:51 +0000

    wtf is happening

--- utils/gpio.js
+++ utils/gpio.js
@@ -1,25 +1,23 @@
 const fs = require("fs");
 
 function setGpio(pin, value, direction = "output") {
-    const writeDirection = `${direction === "output" ? "out" : direction === "input" ? "in" : "out"}\n`;
-    const writeValue = `${value === false ? 0 :  value === true ? 1 : value}\n`;
-
     // Export pin
     if (!fs.existsSync(`/sys/class/gpio/gpio${pin}`)) {
-        fs.writeFileSync("/sys/class/gpio/export", `${pin}\n`);
+        fs.writeFileSync("/sys/class/gpio/export", `${pin}`);
     }
 
     // Set direction
-    // if (fs.readFileSync(`/sys/class/gpio/gpio${pin}/direction`, "utf-8") !== writeDirection) {
-    fs.writeFileSync(`/sys/class/gpio/gpio${pin}/direction`, `${writeDirection}`);
-    // }
-
+    if (fs.readFileSync(`/sys/class/gpio/gpio${pin}/direction`, "utf-8") !== writeDirection) {
+        fs.writeFileSync(`/sys/class/gpio/gpio${pin}/direction`, direction === "output" ? "out" : direction === "input" ? "in" : "out");
+    }
+    
     // Set value
-    fs.writeFileSync(`/sys/class/gpio/gpio${pin}/value`, `${writeValue}`);
+    fs.writeFileSync(`/sys/class/gpio/gpio${pin}/value`, `${value === false ? 0 :  value === true ? 1 : value}`);
 }
 
 function getGpioValue(pin) {
     if (!fs.existsSync(`/sys/class/gpio/gpio${pin}`)) return null;
+    fs.writeFileSync(`/sys/class/gpio/gpio${pin}/direction`, `out`);
     return parseInt(fs.readFileSync(`/sys/class/gpio/gpio${pin}/value`, "utf-8"));
 }
 
